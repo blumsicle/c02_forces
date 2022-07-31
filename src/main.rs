c417704dfe5069676fcfa4487aa8150e5f2eb3b6
Brian Blumberg <blumsicle@gmail.com>
2022-07-30 18:29:42 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use nannou::prelude::*;
+
+struct Model {}
+
+fn model(app: &App) -> Model {
+    let _ = app
+        .new_window()
+        .title("noc_c02_forces")
+        .size(800, 800)
+        .view(view)
+        .build()
+        .unwrap();
+
+    Model {}
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
+fn view(app: &App, _model: &Model, frame: Frame) {
+    let draw = app.draw();
+
+    draw.background().color(hsv(0.5, 0.4, 0.4));
+
+    draw.to_frame(app, &frame).unwrap();
+}
+
+fn main() {
+    nannou::app(model).update(update).run();
+}
