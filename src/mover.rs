01b1f1f9089644f121ff1c19dd856f5736ee4e1f
Brian Blumberg <blumsicle@gmail.com>
2022-08-05 19:06:07 -0700

    Remove upper bounds

--- src/mover.rs
+++ src/mover.rs
@@ -61,9 +61,6 @@ impl Mover {
         if self.position.y - self.radius < bounds.bottom() {
             self.position.y = bounds.bottom() + self.radius;
             self.velocity.y *= -1.0;
-        } else if self.position.y + self.radius > bounds.top() {
-            self.position.y = bounds.top() - self.radius;
-            self.velocity.y *= -1.0;
         }
     }
 }
